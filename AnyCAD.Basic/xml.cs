b237cbbcc2da21bcc0a482a3ee7624809d118ec8
Anthony Zhu <anthonyzhu268524@gmail.com>
2020-06-19 11:23:44 +0800

    Test MessageBox

--- AnyCAD.Basic/xml.cs
+++ AnyCAD.Basic/xml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -29,16 +30,6 @@ namespace AnyCAD.Basic
         public double Angle;
         public double Radius;
         public double Length;
-
-        //public Bending(EnumEdge or, int id, EnumDir dir, double ag, double ra, double le)
-        //{
-        //    Orientation = or;
-        //    Index = id;
-        //    Direction = dir;
-        //    Angle = ag;
-        //    Radius = ra;
-        //    Length = le;
-        //}
     }
     public class BengdingGroup
     {
@@ -117,6 +108,7 @@ namespace AnyCAD.Basic
             var path = new System.IO.StreamWriter(@"c:\batch\Serialization.xml");
             writer.Serialize(path, bendings);
             path.Close();
+            MessageBox.Show("输出成功！", "输出提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
         public static void ReadXml()
         {
@@ -124,7 +116,7 @@ namespace AnyCAD.Basic
             var path = new System.IO.StreamReader(@"c:\batch\Serialization.xml");
             BengdingGroup bendings = reader.Deserialize(path) as BengdingGroup;
             path.Close();
-            Console.WriteLine(bendings.bendings.Last().Index);
+            MessageBox.Show("Last id: " + bendings.bendings.Last().Index.ToString(), "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
